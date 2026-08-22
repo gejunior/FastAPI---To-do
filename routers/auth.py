b6f051bd0db0c4dce917befff08bc80212ca0b13
genilson <genilson_junior14@outlook.com>
2026-08-21 21:06:01 -0300

    adicionando hash nas senhas

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from passlib.utils.binary import BCRYPT_CHARS
 from pydantic import BaseModel
+from sqlalchemy.util import deprecated
+
 from models import Users
+from passlib.context import CryptContext
+
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -11,17 +18,17 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
-@router.post("/auth/")
+@router.post("/auth")
 async def create_user(create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
 
 
-
-    return{'user': 'authenticated'}
+    return create_user_model
+    # return{'user': 'authenticated'}
