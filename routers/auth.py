e9b15a7c94d3fb2ca1b6ac00c4c42bce6fdbb8ad
genilson <genilson_junior14@outlook.com>
2026-08-21 21:29:01 -0300

    adicionando usuario ao banco de dados

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
-from passlib.utils.binary import BCRYPT_CHARS
+from typing import Annotated
+from fastapi import APIRouter,Depends
 from pydantic import BaseModel
-from sqlalchemy.util import deprecated
+from sqlalchemy.orm import Session
+from starlette import status
 
+from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 
@@ -18,8 +20,18 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -30,5 +42,5 @@ async def create_user(create_user_request: CreateUserRequest):
     )
 
 
-    return create_user_model
-    # return{'user': 'authenticated'}
+    db.add(create_user_model)
+    db.commit()
